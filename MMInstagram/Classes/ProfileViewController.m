fd8e4a444428b14fe490a9598886ab010becb57d
AndrewDolly <andrew.dahle@gmail.com>
2015-04-09 15:48:55 -0700

    search working

--- MMInstagram/Classes/ProfileViewController.m
+++ MMInstagram/Classes/ProfileViewController.m
@@ -15,6 +15,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *postsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *followersLabel;
 @property (weak, nonatomic) IBOutlet UILabel *followingLabel;
+@property (nonatomic) UIImage *selectedImage;
+@property NSString *userName;
 
 
 @end
@@ -24,7 +26,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self getProfile];
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(selectPic:)];
+    [singleTap setNumberOfTapsRequired:1];
+    singleTap.delegate = self;
+    [self.view addGestureRecognizer:singleTap];
+
 }
+
+
+
 
 - (void)getProfile {
     PFUser *current = [PFUser currentUser];
@@ -41,68 +51,74 @@
     }
 }
 
+- (void)getUserName {
+    PFUser *current = [PFUser currentUser];
+        self.userName = current[@"username"];
+}
+
+
 #pragma mark - UIImagePickerDelegate
-//- (void)promptForCamera {
-//    UIImagePickerController *controller = [[UIImagePickerController alloc] init];
-//    controller.sourceType = UIImagePickerControllerSourceTypeCamera;
-//    controller.delegate = self;
-//    [self presentViewController:controller animated:YES completion:nil];
-//}
-//
-//- (void)promptForPhotoRoll {
-//    UIImagePickerController *controller = [[UIImagePickerController alloc] init];
-//    controller.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-//    controller.delegate = self;
-//    [self presentViewController:controller animated:YES completion:nil];
-//}
-//
-//- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-//    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
-//    [self.profileImageView setImage:image];
-//    self.selectedImage = image;
-//
-//    [self dismissViewControllerAnimated:YES completion:nil];
-//}
-//
-//- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-//    [self dismissViewControllerAnimated:YES completion:nil];
-//
-//}
-//
-//- (IBAction)selectPic:(UITapGestureRecognizer *)sender {
-//    UIActionSheet *actionSheet = nil;
-//    actionSheet = [[UIActionSheet alloc]initWithTitle:nil
-//                                             delegate:self cancelButtonTitle:nil
-//                               destructiveButtonTitle:nil
-//                                    otherButtonTitles:nil];
-//
-//    // only add avaliable source to actionsheet
-//    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]){
-//        [actionSheet addButtonWithTitle:@"Photo Library"];
-//    }
-//    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
-//        [actionSheet addButtonWithTitle:@"Camera Roll"];
-//    }
-//
-//    [actionSheet addButtonWithTitle:@"Cancel"];
-//    [actionSheet setCancelButtonIndex:actionSheet.numberOfButtons-1];
-//
-//    [actionSheet showInView:self.navigationController.view];
-//}
-//
-//- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
-//    if (buttonIndex != actionSheet.cancelButtonIndex) {
-//        if (buttonIndex != actionSheet.firstOtherButtonIndex) {
-//            [self promptForPhotoRoll];
-//        } else {
-//            [self promptForCamera];
-//        }
-//    }
-//}
-//
-//
-//- (IBAction)onCancelButtonTapped:(UIBarButtonItem *)sender {
-//    [self dismissViewControllerAnimated:YES completion:nil];
-//}
+
+- (void)promptForCamera {
+    UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+    controller.sourceType = UIImagePickerControllerSourceTypeCamera;
+    controller.delegate = self;
+    [self presentViewController:controller animated:YES completion:nil];
+}
+- (void)promptForPhotoRoll {
+    UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+    controller.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    controller.delegate = self;
+    [self presentViewController:controller animated:YES completion:nil];
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    [self.profileImageView setImage:image];
+    self.selectedImage = image;
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)selectPic:(UITapGestureRecognizer *)sender {
+    UIActionSheet *actionSheet = nil;
+    actionSheet = [[UIActionSheet alloc]initWithTitle:nil
+                                             delegate:self cancelButtonTitle:nil
+                               destructiveButtonTitle:nil
+                                    otherButtonTitles:nil];
+    // only add avaliable source to actionsheet
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]){
+        [actionSheet addButtonWithTitle:@"Photo Library"];
+    }
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        [actionSheet addButtonWithTitle:@"Camera Roll"];
+    }
+    [actionSheet addButtonWithTitle:@"Cancel"];
+    [actionSheet setCancelButtonIndex:actionSheet.numberOfButtons-1];
+    [actionSheet showInView:self.navigationController.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex != actionSheet.cancelButtonIndex) {
+        if (buttonIndex != actionSheet.firstOtherButtonIndex) {
+            [self promptForPhotoRoll];
+        } else {
+            [self promptForCamera];
+        }
+    }
+}
+
+
+
+
+- (IBAction)unwindFromWebView:(UIStoryboardSegue *)sender
+{
+
+}- (IBAction)onCancelButtonTapped:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
--- MMInstagram/EditProfileViewController.m
+++ MMInstagram/EditProfileViewController.m
@@ -7,16 +7,17 @@
 //
 
 #import "EditProfileViewController.h"
+#import "User.h"
+#import "Photo.h"
+#import "SignUpViewController.h"
+#import "ProfileViewController.h"
 
 @interface EditProfileViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIActionSheetDelegate, UIGestureRecognizerDelegate, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *nameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
-@property (weak, nonatomic) IBOutlet UITextField *websiteTextField;
 @property (weak, nonatomic) IBOutlet UITextField *bioTextField;
-@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
-
-//@property (weak, nonatomic) IBOutlet UIImageView *characterImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 //@property (nonatomic) UIImage *selectedImage;
 @property BOOL isEditSelected;
 
@@ -28,19 +29,145 @@
     self.isEditSelected = NO;
     self.nameTextField.enabled = self.isEditSelected;
     self.usernameTextField.enabled = self.isEditSelected;
-    self.websiteTextField.enabled = self.isEditSelected;
-    self.bioTextField.enabled = self.isEditSelected;
-    self.phoneTextField.enabled = self.isEditSelected;
+    [self loadFields];
+
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(selectPic:)];
+    [singleTap setNumberOfTapsRequired:1];
+    singleTap.delegate = self;
+    [self.view addGestureRecognizer:singleTap];
 }
 
-//- (void)loadFields {
-//    self.nameTextField.text = [self.selectedCharacter valueForKey:@"name"];
-//    self.actorTextField.text = [self.selectedCharacter valueForKey:@"actor"];
-//    self.seatTextField.text = [NSString stringWithFormat:@"%@",[self.selectedCharacter valueForKey:@"seat"]];
-//    self.genderTextField.text = [self.selectedCharacter valueForKey:@"gender"];
-//    self.characterImageView.image = [UIImage imageWithData:[self.selectedCharacter valueForKey:@"image"]];
+//
+//- (BOOL)textFieldShouldReturn:(UITextField *)textField
+//{
+//    [self saveData];
+//    return YES;
+//}
+//
+//
+
+- (void)loadFields {
+    PFUser *current = [PFUser currentUser];
+        self.nameTextField.text = current[@"name"];
+        self.usernameTextField.text = current[@"username"];
+//        PFFile *imageData = [current objectForKey:@"profileImage"];
+//        [imageData getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+//        self.profileImageView.image = [UIImage imageWithData:data];
+}
+
+
+- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+//    if (!self.isEditSelected) {
+//        sender.title = @"Done";
+//        [self.selectedCharacter setValue:self.nameTextField.text forKey:@"name"];
+//        [self.selectedCharacter setValue:self.actorTextField.text forKey:@"actor"];
+//        [self.selectedCharacter setValue:self.genderTextField.text forKey:@"gender"];
+//
+//        NSNumber *seat = [NSNumber numberWithInt:(int)self.seatTextField.text.integerValue];
+//        [self.selectedCharacter setValue:seat forKey:@"seat"];
+//
+//        [self.moc save:nil];
+//    } else {
+//        sender.title = @"Edit";
+//    }
+//
+//    self.nameTextField.enabled = !self.isEditSelected;
+//    self.actorTextField.enabled = !self.isEditSelected;
+//    self.seatTextField.enabled = !self.isEditSelected;
+//    self.isEditSelected = !self.isEditSelected;
+
+}
+//
+//-(void)textFieldDidEndEditing:(UITextField *)textField {
+//    if (textField == self.nameTextField)
+//    {
+//        [self.selectedCharacter setValue:self.nameTextField.text forKey:@"name"];
+//    }
+//    if (textField == self.actorTextField) {
+//        [self.selectedCharacter setValue:self.actorTextField.text forKey:@"actor"];
+//    }
+//    if (textField == self.seatTextField) {
+//
+//        NSNumber *seat = [NSNumber numberWithInt:(int)self.seatTextField.text.integerValue];
+//        [self.selectedCharacter setValue:seat forKey:@"seat"];
+//    }
+//    if (textField == self.genderTextField) {
+//        [self.selectedCharacter setValue:self.genderTextField.text forKey:@"gender"];
+//    }
+//
+//    [self.moc save:nil];
 //}
 
+//
+//- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+//    if (buttonIndex != actionSheet.cancelButtonIndex) {
+//        if (buttonIndex != actionSheet.firstOtherButtonIndex) {
+//            [self promptForPhotoRoll];
+//        } else {
+//            [self promptForCamera];
+//        }
+//    }
+//}
+//
+//- (void)saveData
+//{
+//    [self.nameTextField resignFirstResponder];
+//    [self.usernameTextField resignFirstResponder];
+//
+//    if ([self.nameTextField.text length] > 0 && [self.usernameTextField.text length] > 0) {
+//        self.name = self.nameLabel.text = self.nameTextField.text;
+//        self.city.stateOrProvince = self.stateOrProvinceLabel.text = self.stateOrProvinceTextField.text;
+//    }
+//}
+//
+//
+//
 
+
+
+//
+//#pragma mark - UItextField
+//
+//- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+//    if (!self.isEditSelected) {
+//        sender.title = @"Done";
+//        [self.selectedCharacter setValue:self.nameTextField.text forKey:@"name"];
+//        [self.selectedCharacter setValue:self.actorTextField.text forKey:@"actor"];
+//        [self.selectedCharacter setValue:self.genderTextField.text forKey:@"gender"];
+//
+//        NSNumber *seat = [NSNumber numberWithInt:(int)self.seatTextField.text.integerValue];
+//        [self.selectedCharacter setValue:seat forKey:@"seat"];
+//
+//        [self.moc save:nil];
+//    } else {
+//        sender.title = @"Edit";
+//    }
+//
+//    self.nameTextField.enabled = !self.isEditSelected;
+//    self.actorTextField.enabled = !self.isEditSelected;
+//    self.seatTextField.enabled = !self.isEditSelected;
+//    self.genderTextField.enabled = !self.isEditSelected;
+//    self.isEditSelected = !self.isEditSelected;
+//
+//}
+//
+//-(void)textFieldDidEndEditing:(UITextField *)textField {
+//    if (textField == self.nameTextField)
+//    {
+//        [self.selectedCharacter setValue:self.nameTextField.text forKey:@"name"];
+//    }
+//    if (textField == self.actorTextField) {
+//        [self.selectedCharacter setValue:self.actorTextField.text forKey:@"actor"];
+//    }
+//    if (textField == self.seatTextField) {
+//
+//        NSNumber *seat = [NSNumber numberWithInt:(int)self.seatTextField.text.integerValue];
+//        [self.selectedCharacter setValue:seat forKey:@"seat"];
+//    }
+//    if (textField == self.genderTextField) {
+//        [self.selectedCharacter setValue:self.genderTextField.text forKey:@"gender"];
+//    }
+//
+//    [self.moc save:nil];
 
 @end
